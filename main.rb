168524aac5d8990c8e1edd232e6a8cc17d7833c7
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-07 16:10:15 -0500

    create staff member route

--- main.rb
+++ main.rb
@@ -15,6 +15,9 @@ get '/' do
   erb :root
 end
 
+########################################
+####### LIBRARY ########################
+########################################
 #libraries menu
 get '/libraries' do
   erb :libraries_menu
@@ -30,4 +33,30 @@ end
 get '/library/:id' do
   @library = Library.find_by_id(params['id'])
   erb :library_show
+end
+
+#library edit
+get '/library/:id/edit' do
+  @library = Library.find_by_id(params['id'])
+  erb :library_edit
+end
+
+post '/library/:id/update' do
+  @library = Library.find_by_id(params['id'])
+  if @library.update_attributes(branch_name: params['branch_name'],
+                                address: params['address'],
+                                phone_number: params['phone_number'])
+    redirect to ("/library/#{@library.id}")
+  else
+    erb :library_edit
+  end
+end
+
+########################################
+####### Staff Member ###################
+########################################
+
+#libraries menu
+get '/staff_members' do
+  erb :staff_members_menu
 end
