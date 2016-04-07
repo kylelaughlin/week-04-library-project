34a8859d854da47132729fad96e44365c18b001f
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-07 11:45:52 -0500

    Updated database schema to include foreign keys

---
+++ db/migrate/002_make_foreign_key_columns.rb
@@ -0,0 +1,9 @@
+class MakeForeignKeyColumns < ActiveRecord::Migration
+  def change
+    add_column :books, :library_id, :integer
+
+    add_column :books, :patron_id, :integer
+
+    add_column :staff_members, :library_id, :integer
+  end
+end
