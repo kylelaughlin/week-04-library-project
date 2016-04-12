2420c60a38f268971f805182059b71633c46a022
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-12 08:54:01 -0500

    move libraries views to libraries directory

--- main.rb
+++ main.rb
@@ -26,12 +26,12 @@ end
 #libraries index
 get '/libraries' do
   @libraries = Library.all
-  erb :libraries_index
+  erb :"libraries/index"
 end
 
 get '/libraries/new' do
   @library = Library.new
-  erb :library_new
+  erb :"libraries/new"
 end
 
 post '/libraries' do
@@ -39,7 +39,7 @@ post '/libraries' do
   if @library.save
     redirect to("/libraries")
   else
-    erb :library_new
+    erb :"libraries/new"
   end
 end
 
@@ -48,31 +48,30 @@ get '/libraries/:id' do
   @library = Library.find_by_id(params['id'])
   @staff_members = StaffMember.where(library_id: params['id'])
   @books = Book.where(library_id: params['id'])
-  erb :library_show
+  erb :"libraries/show"
 end
 
 #library edit
 get '/libraries/:id/edit' do
   @library = Library.find_by_id(params['id'])
-  erb :library_edit
+  erb :"libraries/edit"
 end
 
 post '/libraries/:id' do
-  binding.pry
   @library = Library.find_by_id(params['id'])
   if @library.update_attributes(branch_name: params['branch_name'],
                                 address: params['address'],
                                 phone_number: params['phone_number'])
     redirect to ("/libraries/#{@library.id}")
   else
-    erb :library_edit
+    erb :"libraries/edit"
   end
 end
 
 get '/libraries/:id/staff_members' do
   @library = Library.find_by_id(params['id'])
   @staff_members = StaffMember.where(library_id: params['id'])
-  erb :library_staff_members
+  erb :"libraries/staff_members"
 end
 
 ########################################
