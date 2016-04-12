26c5251850ce4f5b9980d556f68d88ee83811398
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-12 14:02:37 -0500

    add tests to models folder

--- main.rb
+++ main.rb
@@ -3,13 +3,8 @@ require "sinatra"
 require "sinatra/activerecord"
 require "sinatra/reloader"
 
-require_relative "lib/book.rb"
-require_relative "lib/library.rb"
-require_relative "lib/patron.rb"
-require_relative "lib/staff_member.rb"
-
 Dir[File.dirname(__FILE__) + '/app/controllers/*.rb'].each {|file| require file }
-
+Dir[File.dirname(__FILE__) + '/app/models/*.rb'].each {|file| require file}
 #Root
 
 get '/pry' do
